628690d282b94ddcd51cdc9eef8addddd7c514fd
TR-76 <tr-76@gmx.at>
2022-03-11 09:34:07 +0100

    Add MG-APKS
    Signed-off-by: TR-76 <tr-76@gmx.at>

--- lineage_vayu.mk
+++ lineage_vayu.mk
@@ -14,6 +14,9 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 # Inherit from vayu device
 $(call inherit-product, device/xiaomi/vayu/device.mk)
 
+# MG-APKS
+$(call inherit-product-if-exists, vendor/mg-apks/config.mk)
+
 PRODUCT_NAME := lineage_vayu
 PRODUCT_DEVICE := vayu
 PRODUCT_MANUFACTURER := Xiaomi
